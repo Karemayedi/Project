a692fc45693df9b22c523d211b6b56e50ee01b34
ASUS <ASUS@DESKTOP-9EVNMU4>
2020-11-03 15:53:06 +0100

    added class Administrateur.java

---
+++ ProjectVermeg/src/Administrateur.java
@@ -0,0 +1,9 @@
+
+
+public class Administrateur extends Personne {
+
+	public Administrateur(int id, String nom, String prenom) {
+		super(id, nom, prenom);
+		// TODO Auto-generated constructor stub
+	}
+}
